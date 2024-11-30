4cb772435945afa02e051ef46ce549b25c557bba
Libin792814829 <lb18790140172@163.com>
2024-12-01 00:23:21 +0800

    chore(): 修改依赖

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "normalize.css";
-import "./utils/element";
+import Vue from 'vue';
+import App from './App.vue';
+import 'normalize.css';
+import './utils/element';
 Vue.config.productionTip = false;
 
-Vue.prototype.$ELEMENT = { size: "small" };
+Vue.prototype.$ELEMENT = { size: 'small' };
 new Vue({
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
